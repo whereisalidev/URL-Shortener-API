cbbfc7d5a534bc8f25d44b2b6b988de3a48b1c09
ali-jee-786 <786syedalihussain@gmail.com>
2024-11-09 10:52:34 +0500

    updated

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class UrlShortenerModel(models.Model):
+    original_url = models.TextField()
+    short_url = models.CharField(max_length=30, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
